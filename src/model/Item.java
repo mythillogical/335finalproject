8604cc2dbef44e9bc3728d92bce41dddae284f29
mohad1122 <maa111@arizona.edu>
2025-04-11 12:34:35 -0700

    create Modification and Item

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,8 +1,38 @@
 package model;
 
-public class Item {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Item {
+    private String name;
+    private String category;
+    private double baseCost;
+    private List<Modification> modifications;
+
+    public Item(String name, String category, double baseCost) {
+        this.name = name;
+        this.category = category;
+        this.baseCost = baseCost;
+        this.modifications = new ArrayList<>();
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
     public double getCost() {
-        return 0;
+    	double totalCost = baseCost;
+        for (Modification mod : modifications) {
+        	totalCost += mod.getAdditionalCost();
+        }
+        return totalCost;
+    }
+
+    public void addModification(Modification mod) {
+        modifications.add(mod);
     }
 }
--- src/model/Modification.java
+++ src/model/Modification.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Modification {
-
+	private String description;
+	private double additionalCost;
+	
+	public Modification(String description, double additionalCost) {
+		this.description = description;
+		this.additionalCost = additionalCost;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public double getAdditionalCost() {
+		return this.additionalCost;
+	}
 }
