fea854bd3c32d189fcad3851ed37549ecca6a627
mohad1122 <maa111@arizona.edu>
2025-04-10 20:37:42 -0700

    create sever
    I add garters and addTips to Server class

--- src/model/Server.java
+++ src/model/Server.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Server {
-
+	private String serverID;
+	private String name;
+	private double totalTips;
+	
+	public Server (String serverID, String name) {
+		this.serverID = serverID;
+		this.name = name;
+		this.totalTips = 0;
+	}
+	public void addTips(double amount) {
+		this.totalTips += amount; 
+	}
+	public String getServerID() {
+		return serverID;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getTotalTips() {
+		return totalTips;
+	}
 }
