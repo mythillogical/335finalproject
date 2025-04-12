6b56ac786546dc56d766a329bdfc68320e5b27e2
mohad1122 <maa111@arizona.edu>
2025-04-11 20:22:35 -0700

    change menuItemTest

--- src/test/MenuItemTest.java
+++ src/test/MenuItemTest.java
@@ -1,24 +1,17 @@
 package test;
-import model.Item;
 import model.MenuItem;
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 class MenuItemTest {
 
-	/*
+	
 	@Test
 	void testProccecingFile() {
 		MenuItem m = new MenuItem("Menu.csv");
 		m.getMenuMap();
-	}*/
-	
-	public static void main(String[] args) {
-	    MenuItem m = new MenuItem("Menu.csv");
-	    System.out.println(m);
 	}
 
 }
