c8fc1168f973204482d882ccfeb092c8551aefef
mohad1122 <maa111@arizona.edu>
2025-04-19 18:18:53 -0700

    add RestaurantView

---
+++ src/view/RestaurantView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.List;
+import model.*;
+
+public class RestaurantView {
+
+    public void displayTables(List<Table> tables) {
+    	
+    }
+
+    public void displayMenu(List<Item> menu) {
+        
+    }
+
+    public void displayBill(int tableNumber, Bill bill) {
+        
+    }
+
+    //public void displaySalesReport(Map<MenuItem, Integer> sales) {
+    //}
+
+    public void displayError(String message) {
+    	
+    }
+}
