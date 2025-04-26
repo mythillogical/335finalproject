1b5f299670558ef718da4d6c2b698086eaf5ca38
Asifur Rahman <120474868+asifrahman2003@users.noreply.github.com>
2025-04-25 17:46:54 -0700

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,16 @@
 package view;
 
-// launcher class to start the restaurant gui
+/* Main entry point for the Restaurant Management System application.
+* 
+* Initializes the graphical user interface (GUI) and links the 
+* model, view, and controller components following the MVC pattern.
+* 
+* This class creates the main application window and starts the program.
+* 
+* Author: Michael B, Michael D, Asif R, Mohammed A
+* Course: CSc 335 - Object-Oriented Programming and Design
+* Semester: Spring 2025
+*/
 public class Main {
 	public static void main(String[] args) {
 		// delegate to RestaurantGUI
