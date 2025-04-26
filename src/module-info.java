b6ec2d182a56f384fa01298614364b4e3df053fe
Asifur Rahman <120474868+asifrahman2003@users.noreply.github.com>
2025-04-25 18:01:00 -0700

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,13 @@
 /**
+ * Module definition for the Restaurant Management System (Rest-O-Bot).
  * 
- */
-/**
+ * Specifies required dependencies for the project:
+ * - JUnit 5 for unit testing
+ * - Java Desktop APIs (Swing) for GUI development
  * 
+ * Author: Michael B, Michael D, Asif R, Mohammed A
+ * Course: CSc 335 - Object-Oriented Programming and Design
+ * Semester: Spring 2025
  */
 module rest_o_bot {
 	requires org.junit.jupiter.api;
